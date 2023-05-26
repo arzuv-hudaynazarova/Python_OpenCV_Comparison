fe6ef3b8d8a09401251abf320143ed3be6960cdb
Arzuv Hudaynazarova <126810475+arzuv-hudaynazarova@users.noreply.github.com>
2023-05-26 15:12:01 +0300

    Update image_comprasion.py

--- image_comprasion.py
+++ image_comprasion.py
@@ -4,7 +4,7 @@ import time
 import numpy as np
 from skimage.metrics import structural_similarity as ssim
 
-# Belirtilen klasörden resimleri yükleris
+# Belirtilen klasörden resimleri yükleriz
 def load_images_from_folder(folder):
     images = []
     for filename in os.listdir(folder):
@@ -14,7 +14,7 @@ def load_images_from_folder(folder):
     return images
 
 
-# İki resim arasındaki benzerlik skorunu hesaplarıs
+# İki resim arasındaki benzerlik skorunu hesaplarız
 def calculate_similarity(imageA, imageB):
     imageA = cv2.cvtColor(imageA, cv2.COLOR_BGR2GRAY)
     imageB = cv2.cvtColor(imageB, cv2.COLOR_BGR2GRAY)
@@ -29,7 +29,7 @@ def calculate_similarity(imageA, imageB):
     return ssim(imageA, imageB)
 
 
-# Görüntüler arasındaki benzerlik skorlarını bulurus
+# Görüntüler arasındaki benzerlik skorlarını buluruz
 def find_similar_images(images):
     similarity_list = []
     for i in range(len(images)):
@@ -42,21 +42,22 @@ def find_similar_images(images):
 
     return similarity_list
 
-# Belirtilen klasördeki görüntüler arasında benzer olanları bulurus
+# Belirtilen klasördeki görüntüler arasında benzer olanları buluruz
 def find_similar_images_in_folder(folder_path):
     images = load_images_from_folder(folder_path)
     similarity_list = find_similar_images(images)
     return similarity_list
 
-folder_path =  r"C:\Users\leyla\Downloads\pythonProject2\ALGORTIMA_ANALIZI_ODEV_SOURCES"
+#asagıdaki folder_path degiskenine karsılastırmak istediginiz resimlerin bulundugu dosyanın konumunu yazınız
+folder_path =  r"C:\Users\yusuf\OneDrive\Masaüstü\AAKS2_2\ALGORTIMA_ANALIZI_ODEV_SOURCES-20230523T105200Z-001"
 
-baslama_zamani = time.time()
+baslama_zamani = time.time()# fonksiyonu başlatmadan önceki anın zamanını alır
 similarity_list = find_similar_images_in_folder(folder_path)
-bitis_zamani = time.time()
+bitis_zamani = time.time()# fonksiyonun bitişindeki anın zamanını alır
 
-# Benzerlik skoru tam 1 olan görüntü çiftlerini ekranda yazdırırıs
+# Benzerlik skoru tam 1 olan görüntü çiftlerini ekranda yazdırırız
 for i, j, score in similarity_list:
-    if score == 1.0:
+    if score == 1.0: # score değerinin 1 olanlar yazılacaktır eğer belli bir seviyeden yüksek olanları yazdırmak isterseniz >= 0.8 benzeri bir yazımla bunu gerçekleştirebilirsiniz.
         print(f"Görüntüler {i} ve {j} benzerlik skoru {score} ile benzer.")
 
 # Toplamda kaç karşılaştırma yapıldığını hesaplarıs
@@ -65,13 +66,5 @@ comparisons = num_images * (num_images - 1) / 2
 print(f"Görseller arasındaki toplam karşılaştırmalar: {int(comparisons)}")
 
 # Hesaplamanın ne kadar süre aldığını yazdırırıs
-print(f"Harcanan süre: {bitis_zamani - baslama_zamani} saniye")
-
-
-
-
-
-
-
-
+print(f"Harcanan süre: {bitis_zamani - baslama_zamani} saniye")
 
